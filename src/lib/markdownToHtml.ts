41399fddad033580464bda4a68b6c65b856334c3
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-22 23:59:35 +0900

    feat: Enhance markdown processing with rehype-pretty-code and additional plugins
    - Integrate rehype-pretty-code for advanced code syntax highlighting with github-dark theme.
    - Add rehypeSlug and rehypeAutolinkHeadings for automatic ID slug generation and linking in headings.
    - Enable GitHub Flavored Markdown (GFM) support with remarkGfm for improved markdown rendering.

--- src/lib/markdownToHtml.ts
+++ src/lib/markdownToHtml.ts
@@ -1,7 +1,21 @@
-import { remark } from 'remark';
-import html from 'remark-html';
+import rehypeSlug from 'rehype-slug';
+import rehypeAutolinkHeadings from 'rehype-autolink-headings';
+import remarkGfm from 'remark-gfm';
+import { unified } from 'unified';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import rehypeStringify from 'rehype-stringify';
+import rehypePrettyCode from 'rehype-pretty-code';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse) // 마크다운 파싱
+    .use(remarkGfm) // GitHub Flavored Markdown 활성화
+    .use(remarkRehype) // 마크다운을 HTML로 변환
+    .use(rehypePrettyCode, { theme: 'github-dark' })
+    .use(rehypeSlug) // 제목에 ID 슬러그 생성
+    .use(rehypeAutolinkHeadings, { behavior: 'prepend' }) // 제목에 자동 링크 추가
+    .use(rehypeStringify) // HTML 문자열로 변환
+    .process(markdown);
   return result.toString();
 }
