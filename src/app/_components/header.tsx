9a4288b8d1c1d6026170afd9a956917619477756
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-26 19:16:55 +0900

    refactor: update header component with improved layout and navigation links

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,11 +2,22 @@ import Link from 'next/link';
 
 const Header = () => {
   return (
-    <h2 className="text-2xl md:text-4xl font-bold tracking-tight md:tracking-tighter leading-tight mb-20 mt-8">
-      <Link href="/" className="hover:underline">
-        Blog
-      </Link>
-      .
+    <h2 className="flex justify-between mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-3xl md:tracking-tighter">
+      <Link href="/">mtseo</Link>
+      <div className="flex justify-start">
+        <Link
+          href="/blog"
+          className="font-normal text-gray-400 transition-colors duration-300 hover:text-gray-900"
+        >
+          blog
+        </Link>
+        <Link
+          href="/projects"
+          className="ml-4 font-normal text-gray-400 transition-colors duration-300 hover:text-gray-900"
+        >
+          Projects
+        </Link>
+      </div>
     </h2>
   );
 };
