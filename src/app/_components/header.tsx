bef364a6d1a9563befea8e87f1c48ab0e1d38de1
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-26 21:37:44 +0900

    feat: Enhance Header component with dynamic navigation links

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -1,22 +1,36 @@
+'use client';
+
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+
+type HeaderMenuTypes = {
+  path: string;
+  name: string;
+};
+
+const headerMenu: HeaderMenuTypes[] = [
+  { path: '/blog', name: 'Blog' },
+  { path: '/projects', name: 'Projects' },
+];
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <h2 className="flex justify-between mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-3xl md:tracking-tighter">
       <Link href="/">mtseo</Link>
       <div className="flex justify-start">
-        <Link
-          href="/blog"
-          className="font-normal text-gray-400 transition-colors duration-300 hover:text-gray-900"
-        >
-          blog
-        </Link>
-        <Link
-          href="/projects"
-          className="ml-4 font-normal text-gray-400 transition-colors duration-300 hover:text-gray-900"
-        >
-          Projects
-        </Link>
+        {headerMenu.map((menu, i) => (
+          <Link
+            key={menu.path}
+            href={`${menu.path}`}
+            className={`${i > 0 ? `ml-4` : ``} ${
+              menu.path === pathname ? `text-gray-900` : `text-gray-400`
+            } font-normal text-gray-400 transition-colors duration-300 hover:text-gray-900`}
+          >
+            {menu.name}
+          </Link>
+        ))}
       </div>
     </h2>
   );
