991746586dbf6c7ca44bdc527d8b211966b3e0cf
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-28 17:51:58 +0900

    rename component 'more-stories' to 'post-list'

--- src/app/_components/post-list.tsx
+++ src/app/_components/post-list.tsx
@@ -5,13 +5,11 @@ type Props = {
   posts: Post[];
 };
 
-export function MoreStories({ posts }: Props) {
+export function PostList({ posts }: Props) {
   return (
-    <section>
-      <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        More Stories
-      </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32 mb-32">
+    <section className="max-w-2xl mx-auto">
+      {/* <div className="grid grid-cols-1 mb-32 md:grid-cols-2 md:gap-x-16 lg:gap-x-32 gap-y-20 md:gap-y-32"> */}
+      <div className="flex-col my-6">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
