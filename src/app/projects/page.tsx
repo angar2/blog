91d8838b70fcd8a878feeaebcd686b5c6d13a4cd
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-28 17:48:09 +0900

    create projects page

---
+++ src/app/projects/page.tsx
@@ -0,0 +1,12 @@
+import Container from '@/app/_components/container';
+import Header from '../_components/header';
+
+export default function ProjectsMain() {
+  return (
+    <main>
+      <Container>
+        <Header />
+      </Container>
+    </main>
+  );
+}
