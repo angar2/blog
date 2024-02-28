bc025cc885f6e53f5a4bde9964c5f05881cc15f7
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-02-28 18:41:55 +0900

    update PostPreview layout and routing
    - Add animation and adjust margins for cleaner layout.
    - Comment out CoverImage and Avatar for simplification.
    - Update heading sizes and excerpt styling for better readability.
    - Change routing from "/posts/[slug]" to "/blog/[slug]".
    - Improve date styling with gray text.

--- src/app/_components/post-preview.tsx
+++ src/app/_components/post-preview.tsx
@@ -22,24 +22,25 @@ export function PostPreview({
   slug,
 }: Props) {
   return (
-    <div>
-      <div className="mb-5">
+    <div className="my-6 animate-slideUp">
+      {/* <div className="mb-5">
         <CoverImage slug={slug} title={title} src={coverImage} />
-      </div>
-      <h3 className="text-3xl mb-3 leading-snug">
+      </div> */}
+      <h3 className="mb-3 text-2xl leading-snug md:text-3xl">
         <Link
-          as={`/posts/${slug}`}
-          href="/posts/[slug]"
+          as={`/blog/${slug}`}
+          href="/blog/[slug]"
           className="hover:underline"
         >
           {title}
         </Link>
       </h3>
-      <div className="text-lg mb-4">
+
+      <p className="mb-3 text-lg leading-relaxed">{excerpt}</p>
+      <div className="text-lg text-gray-500 ">
         <DateFormatter dateString={date} />
       </div>
-      <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
-      <Avatar name={author.name} picture={author.picture} />
+      {/* <Avatar name={author.name} picture={author.picture} /> */}
     </div>
   );
 }
