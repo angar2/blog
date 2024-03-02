9d9f51567bebd757ef7fefd863527fe82fea1985
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-02 13:40:30 +0900

    chore: change og-image

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,3 +1,4 @@
 export const EXAMPLE_PATH = 'blog-starter';
 export const CMS_NAME = 'Markdown';
-export const HOME_OG_IMAGE_URL = '/assets/blog/images/og-image.png';
+export const HOME_OG_IMAGE_URL = '/assets/blog/images/og-image.jpg';
+// export const HOME_OG_IMAGE_URL2 = '/assets/blog/images/og-image.png';
