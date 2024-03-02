7ee4bc500a123e9765efa74a7a85ba328b92fcee
Mintaek.Seo <104804033+MTSEO1@users.noreply.github.com>
2024-03-02 10:57:30 +0900

    fix: og image path

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,3 @@
 export const EXAMPLE_PATH = 'blog-starter';
 export const CMS_NAME = 'Markdown';
-export const HOME_OG_IMAGE_URL =
-  'https://github.com/mtseo01/blog-mtseo/blob/main/public/assets/blog/images/og-image.png?raw=true';
+export const HOME_OG_IMAGE_URL = '/assets/blog/images/og-image.png';
