fec60be196462ed95d63d16f2aeff43bc07ede97
앵가리 <>
2024-03-13 21:53:44 +0900

    chore: Public 구조 변경 및 예시 형식 삭제

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,4 +1,4 @@
-export const EXAMPLE_PATH = 'blog-starter';
-export const CMS_NAME = 'Markdown';
-export const HOME_OG_IMAGE_URL = '/assets/blog/images/og-image.jpg';
-export const LOGO_IMAGE_URL = '/assets/blog/images/logo-image.png';
+export const EXAMPLE_PATH = "blog-starter";
+export const CMS_NAME = "Markdown";
+export const HOME_OG_IMAGE_URL = "/assets/images/og-image.jpg";
+export const LOGO_IMAGE_URL = "/assets/images/logo-image.png";
