1452a417bf26936e415b7f230f43f32ec5ccedd0
앵가리 <>
2024-03-14 01:33:32 +0900

    refactor: 주석 제거

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,4 @@
 import Container from '@/app/_components/container';
-import { HeroPost } from '@/app/_components/hero-post';
-import { Intro } from '@/app/_components/intro';
 import { PostList } from '@/app/_components/post-list';
 import { getAllPosts } from '@/lib/api';
 import Header from '../_components/header';
@@ -14,24 +12,10 @@ export const metadata: Metadata = {
 export default function BlogMain() {
   const allPosts = getAllPosts();
 
-  // const heroPost = allPosts[0];
-
-  // const morePosts = allPosts.slice(1);
-
   return (
     <main>
       <Container>
         <Header />
-        {/* <Intro /> */}
-        {/* <HeroPost
-          title={heroPost.title}
-          coverImage={heroPost.coverImage}
-          date={heroPost.date}
-          author={heroPost.author}
-          slug={heroPost.slug}
-          excerpt={heroPost.excerpt}
-        /> */}
-        {/* {allPosts.length > 0 && <MoreStories posts={allPosts} />} */}
         {allPosts.length > 0 && <PostList posts={allPosts} />}
       </Container>
     </main>
