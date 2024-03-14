57de1d9c5ef56ff289fb5787079df9d03857bc57
앵가리 <>
2024-03-14 19:04:12 +0900

    merge

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -4,12 +4,14 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { LOGO_IMAGE_URL } from '@/lib/constants';
-type HeaderMenuTypes = {
+
+interface HeaderMenuType {
   path: string;
   name: string;
-};
+}
 
-const headerMenu: HeaderMenuTypes[] = [
+const headerMenu: HeaderMenuType[] = [
+  { path: '/experience', name: 'Experience' },
   { path: '/blog', name: 'Blog' },
   { path: '/projects', name: 'Projects' },
 ];
@@ -17,33 +19,37 @@ const headerMenu: HeaderMenuTypes[] = [
 const Header = () => {
   const pathname = usePathname();
 
+  const NavLink = ({ path, name }: HeaderMenuType) => {
+    const isActive = pathname === path || pathname.startsWith(`${path}/`);
+    return (
+      <Link
+        href={path}
+        className={`ml-4 first:ml-0 ${
+          isActive ? 'text-gray-900' : 'text-gray-400'
+        } font-normal transition-colors duration-300 hover:text-gray-900`}
+      >
+        {name}
+      </Link>
+    );
+  };
+
   return (
-    <h2 className='flex justify-between mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-3xl md:tracking-tighter'>
-      <Link href='/'>
+    <header className="flex justify-between mt-8 mb-20 text-2xl font-bold leading-tight tracking-tight md:text-3xl md:tracking-tighter">
+      <Link href="/">
         <Image
-          alt='logo'
+          alt="logo"
           src={LOGO_IMAGE_URL}
           width={192}
           height={192}
           style={{ width: 'auto', height: 82 }}
         />
       </Link>
-      <div className='flex justify-start h-10'>
-        {headerMenu.map((menu, i) => (
-          <Link
-            key={menu.path}
-            href={`${menu.path}`}
-            className={`${i > 0 ? `ml-4` : ``} ${
-              pathname === menu.path || pathname.startsWith(`${menu.path}/`)
-                ? `text-gray-900`
-                : `text-gray-400`
-            } font-normal  text-gray-400 transition-colors duration-300 hover:text-gray-900`}
-          >
-            {menu.name}
-          </Link>
+      <nav className="flex justify-start">
+        {headerMenu.map((menu) => (
+          <NavLink key={menu.path} {...menu} />
         ))}
-      </div>
-    </h2>
+      </nav>
+    </header>
   );
 };
 
