6c39051c257a5be4229f8b345be988840321165a
앵가리 <>
2024-03-25 22:14:04 +0900

    style: Codebox 스타일 변경

--- src/app/_components/project-intro.tsx
+++ src/app/_components/project-intro.tsx
@@ -23,7 +23,7 @@ export default function ProjectIntro({
 
   return (
     <div className="mb-16 px-4 md:px-0">
-      <div className="mb-4">
+      <div className="mb-6">
         <p className="mt-2 py-1 text-2xl font-bold md:mb-2 md:text-3xl">
           {projectName}
         </p>
@@ -35,7 +35,7 @@ export default function ProjectIntro({
         </div>
       </div>
       <div>
-        <div className="my-6">
+        <div className="mb-6">
           <p className="mb-3 ml-1 text-sm font-semibold text-gray-600 italic md:text-base">
             WHAT DID I DO .
           </p>
@@ -50,14 +50,13 @@ export default function ProjectIntro({
             ))}
           </ul>
         </div>
-        <div className="my-8">
-          <p className="mb-1 ml-1 text-sm font-semibold text-green-700 italic md:text-base md:mb-2">
-            TECH STACK .
-          </p>
+        <div className="my-4">
           <div className="flex flex-wrap">
             {techStack.map((text, index) => (
-              <pre className="my-1 mr-2 px-2 bg-gray-100 rounded-md">
-                <code className="text-xs text-gray-800 md:text-sm">{text}</code>
+              <pre className="my-1 mr-2 px-2 border-[0.0625rem] rounded-md hover:border-[#9D4AD1]">
+                <code className="text-xs text-[#9D4AD1] md:text-sm">
+                  {text}
+                </code>
               </pre>
             ))}
           </div>
