adb28e0ee7c4078daff0132441d7778be4ec2c5b
앵가리 <>
2024-04-02 02:27:39 +0900

    feat: Mobile-Nav Footer 위치 조정

--- src/app/_components/header/mobile-nav.tsx
+++ src/app/_components/header/mobile-nav.tsx
@@ -53,7 +53,7 @@ const MobileNav = ({
           );
         })}
       </div>
-      <div className="fixed bottom-8 self-end mr-8">
+      <div className="fixed bottom-8 self-end mr-4">
         <div className="flex flex-col items-end">
           <div className="flex items-center">
             <Link
