27a4f295f1961aec885ed1d025356146cb033066
angar2 <uky00832@gmail.com>
2024-06-06 03:24:24 +0900

    feat: Add Tags in Documents

--- src/app/_components/post/post-header.tsx
+++ src/app/_components/post/post-header.tsx
@@ -3,15 +3,18 @@ import CoverImage from '../common/cover-image';
 import DateFormatter from '../common/date-formatter';
 import { PostTitle } from '@/app/_components/post/post-title';
 import { type Author } from '@/interfaces/author';
+import CodeTag from '../experience/common/code-tag';
+import Link from 'next/link';
 
 type Props = {
   title: string;
   coverImage: string;
   date: string;
   author: Author;
+  tags: string[];
 };
 
-export function PostHeader({ title, coverImage, date, author }: Props) {
+export function PostHeader({ title, coverImage, date, author, tags }: Props) {
   return (
     <>
       <PostTitle>{title}</PostTitle>
@@ -22,8 +25,24 @@ export function PostHeader({ title, coverImage, date, author }: Props) {
         {/* <div className="block md:hidden mb-6">
           <Avatar name={author.name} picture={author.picture} />
         </div> */}
-        <div className="mb-6 text-sm md:mb-8 text-gray-500 md:text-lg">
+        <div className="mb-4 text-sm md:mb-6 text-gray-500 md:text-lg">
           <DateFormatter dateString={date} />
+        </div>
+
+        <div className="flex flex-wrap mb-6 md:mb-8">
+          {/* <CodeTag codes={tagsWithHash} /> */}
+          {tags.map((tag, index) => (
+            <pre
+              key={index}
+              className={`my-1 mr-2 px-2 border-[0.0625rem] rounded-md hover:border-[#9D4AD1]`}
+            >
+              <Link href={`/blog?tag=${tag}`}>
+                <code className={`text-xs text-[#9D4AD1] md:text-sm`}>
+                  #{tag}
+                </code>
+              </Link>
+            </pre>
+          ))}
         </div>
       </div>
     </>
--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -25,6 +25,7 @@ export default async function Post({ params }: Params) {
             coverImage={post.coverImage}
             date={post.date}
             author={post.author}
+            tags={post.tags}
           />
           <PostBody content={content} />
         </article>
