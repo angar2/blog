996d0a34695c3a39e82aa80ce58215aa88ebd257
angar2 <uky00832@gmail.com>
2024-07-16 03:19:09 +0900

    fix: Wrapping in a Suspense tag

--- src/app/project/page.tsx
+++ src/app/project/page.tsx
@@ -2,6 +2,7 @@ import Container from '@/app/_components/container';
 import { Metadata } from 'next';
 import { ProjectList } from '../_components/project/project-list';
 import { getProjects } from '@/lib/api';
+import { Suspense } from 'react';
 
 export const metadata: Metadata = {
   title: 'Projects',
@@ -10,11 +11,15 @@ export const metadata: Metadata = {
 
 export default function ProjectsMain() {
   const allProjects = getProjects();
-  
+
   return (
     <main>
       <Container>
-        <ProjectList allProjects={allProjects} />
+        {allProjects.length > 0 && (
+          <Suspense>
+            <ProjectList allProjects={allProjects} />
+          </Suspense>
+        )}
       </Container>
     </main>
   );
