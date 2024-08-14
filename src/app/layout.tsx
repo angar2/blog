1780f87d4c66b2472867cf788d3687040125f5d8
angar2 <uky00832@gmail.com>
2024-08-14 18:09:12 +0900

    chore: Update web language

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <head>
         <link
           rel="apple-touch-icon"
